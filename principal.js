e1c1db5dc3dee04c433d4051897c70a66dc762de
IzadoraMR <izadora.mirandar@gmail.com>
2021-04-23 00:35:13 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,40 +1,49 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdpeso = document.querySelector(".info-peso");
-var peso = tdpeso.textContent;
+    var paciente = pacientes[i];
 
-var tdaltura = document.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+    var tdpeso = paciente.querySelector(".info-peso");
+    var peso = tdpeso.textContent;
 
-var tdimc = document.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdimc.textContent = imc;
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
-
-
-
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    pesoValido = false;
-    tdimc.textContent = "Peso inválido!";
-}
-if (altura <= 0 || altura >= 3.0) {
-    console.log("Altura inválida");
-    alturaValida = false;
-    tdimc.textContent = "Altura inválida!";
-
-}
-if (alturaValida && pesoValido) {
+    var tdimc = paciente.querySelector(".info-imc");
     var imc = peso / (altura * altura);
     tdimc.textContent = imc;
 
-}
-if (altura <= 0 || altura >= 3.0 && peso <= 0 || peso >= 1000) {
-    console.log("Mais de um valor inválido!");
-    alturaValida = false;
-    pesoValido = false;
-    tdimc.textContent = "Mais de um valor inválido!";
+    var pesoValido = true;
+    var alturaValida = true;
+
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoValido = false;
+        tdimc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0 || altura >= 3.0) {
+        console.log("Altura inválida");
+        alturaValida = false;
+        tdimc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+
+    }
+    if (alturaValida && pesoValido) {
+        var imc = peso / (altura * altura);
+        tdimc.textContent = imc.toFixed(2);
+
+    }
+    var botaoAdd = document.querySelector("#adicionarpaciente");
+
+    botaoAdd.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var form = document.querySelector("formpaciente");
+
+        console.log(form.nome.value)
+
+    });
 
 }
